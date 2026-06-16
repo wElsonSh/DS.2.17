c0ef944bf4af64561195b9813fdeb61d3830cb0c
wElsonSh <seveldaiil@gmail.com>
2026-06-16 12:24:48 +0300

    prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 // @ts-check
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://ds217.dev',
+  site: "https://ds217.dev",
 
-  integrations: [sitemap({
-    serialize(item) {
-      if (item.url === 'https://ds217.dev/') {
-        item.priority = 1;
-      }
-      return item;
-    }
-  })],
+  integrations: [
+    sitemap({
+      serialize(item) {
+        if (item.url === "https://ds217.dev/") {
+          item.priority = 1;
+        }
+        return item;
+      },
+    }),
+  ],
 });
--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,19 +1,19 @@
-import type { APIRoute } from 'astro';
+import type { APIRoute } from "astro";
 
 export const GET: APIRoute = ({ site }) => {
-    // site автоматически берётся из astro.config.mjs
-    const sitemapUrl = new URL('sitemap-index.xml', site).href;
+  // site автоматически берётся из astro.config.mjs
+  const sitemapUrl = new URL("sitemap-index.xml", site).href;
 
-    const robotsTxt = `
+  const robotsTxt = `
 User-agent: *
 Allow: /
 
 Sitemap: ${sitemapUrl}
     `.trim();
 
-    return new Response(robotsTxt, {
-        headers: {
-            'Content-Type': 'text/plain; charset=utf-8',
-        },
-    });
+  return new Response(robotsTxt, {
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8",
+    },
+  });
 };
