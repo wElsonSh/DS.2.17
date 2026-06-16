ee87efa09d5e3f502890707b2a4846cf568c4941
wElsonSh <seveldaiil@gmail.com>
2026-06-16 12:27:27 +0300

    prettierrc config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: "https://ds217.dev",
+  site: 'https://ds217.dev',
 
   integrations: [
     sitemap({
       serialize(item) {
-        if (item.url === "https://ds217.dev/") {
+        if (item.url === 'https://ds217.dev/') {
           item.priority = 1;
         }
         return item;
